16b38e7f73522d3641ec318dd60b756e9213bab7
hasanfuad <hasanmdfuad@hotmail.com>
2021-08-06 21:06:05 +0600

    final commit module-38

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,20 +3,23 @@ import './Products.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
 
 const Products = (props) => {
-    const {name,seller,price,img,stock} = props.product;
+    // console.log(props);
+    const {name,seller,price,img,stock,key} = props.product;
     return (
         <div className="product">
             <div className="product-img">
                 <img src={img} alt="product-images"/>
             </div>
             <div className="product-detail">
-                <h3 style={{color: "purple"}}>{name}</h3>
+                <h3 style={{color: "purple"}}><Link to={"/product/"+key}>{name}</Link></h3>
                 <h3>${price}</h3>
                 <p>by:{seller}</p>
                 <p>Only {stock} left</p>
-                <button onClick={() => props.testing(props.product)} className="add-to-cart-btn"><FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
+                {props.showAddToCart && <button onClick={() => props.testing(props.product)} className="add-to-cart-btn"><FontAwesomeIcon icon={faShoppingCart} />add to cart</button>}
+                
             </div>
         </div>
     );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,20 +4,24 @@ import './Shop.css';
 import fakeData from '../../fakeData'
 import Products from '../Products/Products';
 import Cart from '../Cart/Cart';
+import {addToDatabaseCart} from '../../utilities/databaseManager';
 
 const Shop = () => {
 
     const first10 = fakeData.slice(0,10);
     const [products, setProducts] = useState(first10);
-    console.log(first10);
+    // console.log(first10);
 
     const [cart, setCart] = useState([]);
 
     
     const handleAddCartBtn = ((product)=>{
-        console.log("Product added", product);
+        // console.log("Product added", product);
         const newCart = [...cart, product]
-        setCart(newCart)
+        setCart(newCart);
+        const sameProduct = newCart.filter(pd => pd.key === product.key);
+        const count = sameProduct.length;
+        addToDatabaseCart(product.key, count);
     })
     
 
@@ -25,7 +29,7 @@ const Shop = () => {
         <div className="shop-container">
             <div className="product-container">
             {
-               products.map((pd)=> <Products product={pd} testing={handleAddCartBtn}/>)
+               products.map((pd)=> <Products showAddToCart={true} key={pd.key} product={pd} testing={handleAddCartBtn}/>)
             }
             </div>
             <div className="cart-container">
